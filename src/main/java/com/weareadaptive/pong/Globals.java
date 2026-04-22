8fb3748d28fcaa942b265cd9bd3168bd932f2d23
Pol Curto <pol.curto@weareadaptive.com>
2026-04-22 16:37:16 +0200

    first commit

---
+++ src/main/java/com/weareadaptive/pong/Globals.java
@@ -0,0 +1,24 @@
+package com.weareadaptive.pong;
+
+import io.aeron.ChannelUriStringBuilder;
+import io.aeron.CommonContext;
+
+public class Globals
+{
+    public static int STREAM_ID = 10;
+    public static String AERON_DIR_PATH = "/Volumes/DevShm/aeron-training";
+
+    public static String CHAT_INBOUND_CHANNEL = new ChannelUriStringBuilder()
+            .media(CommonContext.UDP_MEDIA)
+            .endpoint("192.168.1.210:8999")
+            .alias("ChatInboundChannel")
+            .build();
+
+    public static String CHAT_OUTBOUND_CHANNEL = new ChannelUriStringBuilder()
+            .media(CommonContext.UDP_MEDIA)
+            .controlMode(CommonContext.MDC_CONTROL_MODE_DYNAMIC)
+            .controlEndpoint("192.168.1.210:9000")
+            .sessionId(50)
+            .alias("ChatOutboundChannel")
+            .build();
+}
---
+++ src/main/java/com/weareadaptive/pong/agent/AgentState.java
@@ -0,0 +1,11 @@
+package com.weareadaptive.pong.agent;
+
+public enum AgentState
+{
+    INITIAL,
+    STARTING,
+    CONNECTING,
+    STEADY,
+    STOPPED,
+    CLOSED
+}
